013f92a3d80837377ca883e0c61315040b0ca117
FQanbari <fatemeh.ganbari1993@gmail.com>
2022-07-08 20:31:37 +0430

    create apps

---
+++ backend-app/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSpaStaticFiles(options => { options.RootPath = "wwwroot"; });
+builder.Services.AddControllers();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+app.UseSpaStaticFiles();
+app.UseSpa(builder =>
+{
+    if (app.Environment.IsDevelopment())
+    {
+        builder.UseProxyToSpaDevelopmentServer("http://localhost:8081");
+    }
+});
+
+app.Run();
