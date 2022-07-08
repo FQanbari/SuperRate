7c5cd7bd6325ead365995fad4e3b1983dfc33c5e
FQanbari <fatemeh.ganbari1993@gmail.com>
2022-07-08 23:14:12 +0430

    add service for json in backend and corse error when call api

--- backend-app/Program.cs
+++ backend-app/Program.cs
@@ -1,13 +1,32 @@
+using Newtonsoft.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSpaStaticFiles(options => { options.RootPath = "wwwroot"; });
 builder.Services.AddControllers();
+//Json Serializer
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
+options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+    .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyPolicy",
+        policy =>
+        {
+            policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+        });
+});
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
-
+app.UseCors("MyPolicy");
 app.UseRouting();
 
 app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
