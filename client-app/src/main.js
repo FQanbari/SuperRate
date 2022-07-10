bf1cca391ad5f4d7b62572f76ed6d4cb3b0fb6e9
FQanbari <fatemeh.ganbari1993@gmail.com>
2022-07-10 09:52:52 +0430

    change create app vue

--- client-app/src/main.js
+++ client-app/src/main.js
@@ -2,10 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // createApp.config.productionTip = false
-new createApp({
-    render: h => h(App)
-}).$mount('#app');
-//createApp(App).mount('#app')
+
+createApp(App).mount('#app')
 
 
 
